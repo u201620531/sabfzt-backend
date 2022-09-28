9d232c65be21a2b6ccd54df1139efea789723654
Juan Reyes <u201620531@upc.edu.pe>
2022-09-28 11:20:59 -0500

    delete comments

--- src/keys.ts
+++ src/keys.ts
@@ -1,10 +1,10 @@
 import { DB_HOST, DB_PASSWORD, DB_NAME, DB_USER, DB_PORT } from "./config";
 export default {
   database: {
-    host: DB_HOST, //'sabzft-pruebas.mysql.database.azure.com', //'localhost',
-    user: DB_USER, // 'admin_sabzft@sabzft-pruebas',//root',
-    password: DB_PASSWORD, // 'UPCprueba@2022',//'Yasuan100707',
-    port: DB_PORT, // 3306,
-    database: DB_NAME, // 'sabzft-pruebas'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+    database: DB_NAME,
   },
 };
